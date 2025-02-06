7e2b9c25d33424ac4eee2382014fe808e039fd77
MARIE Clément <clement.marie24@gmail.com>
2025-02-06 16:53:54 +0100

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,21 +1,35 @@
 <?php
-
 namespace App\DataFixtures;
-use App\Entity\User;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Citation;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $citations = [
+            ["La vie est un mystère qu'il faut vivre, et non un problème à résoudre.", "Gandhi"],
+            ["Le succès, c'est tomber sept fois, se relever huit.", "Proverbe japonais"],
+            ["Faites que le rêve dévore votre vie afin que la vie ne dévore pas votre rêve.", "Saint-Exupéry"],
+            ["Le seul vrai échec est l'abandon.", "Confucius"],
+            ["Il n'y a pas de hasard, il n'y a que des rendez-vous.", "Paul Éluard"],
+            ["L'imagination est plus importante que le savoir.", "Albert Einstein"],
+            ["La connaissance parle, mais la sagesse écoute.", "Jimi Hendrix"],
+            ["Le bonheur n'est pas quelque chose de prêt à l'emploi. Il vient de vos propres actions.", "Dalai Lama"],
+            ["La plus grande gloire n'est pas de ne jamais tomber, mais de se relever à chaque chute.", "Nelson Mandela"],
+            ["La vie est ce qui arrive quand vous êtes occupé à faire d'autres projets.", "John Lennon"]
+        ];
 
         for ($i = 0; $i < 10; $i++) {
-            $user = new User();
-            $user->setEmail('user' . $i . '@example.com');
-            $manager->persist($user);
+            $data = $citations[array_rand($citations)];
+
+            $citation = new Citation();
+            $citation->setTexte($data[0])
+                ->setAuteur($data[1]);
+
+            $manager->persist($citation);
         }
 
         $manager->flush();
---
+++ src/Entity/Citation.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CitationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CitationRepository::class)]
+class Citation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $text = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $auteur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setTexte(string $test): static
+    {
+        $this->text = $test;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?string $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+}
